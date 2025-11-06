6a9c6288ad943890ac54c27005249df3b93cb893
Aboud <jaouadabdallah76@gmail.com>
2025-11-06 13:53:29 +0000

    feat: Maîtrise des variables et conditions

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Laboratoire prêt !");
+const nom = "Abdallah Jaouad";
+let age = 16;
+ console.log(nom)
+  console.log(age)
+  const motDePasseAttendu = "secret123";
+  let motDePasseUtilisateur = "secret123"
+if (motDePasseUtilisateur === motDePasseAttendu) {
+    console.log("Accès autorisé. Bienvenue !");
+} else if (motDePasseUtilisateur < 8){
+    console.log("Accès refusé. Votre mot de passe est trop court (8 caractères min).");
+} else {
+    console.log("Accès refusé. Mot de passe incorrect.");
+}
